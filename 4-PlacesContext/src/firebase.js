1ee2c21f5a284bf5e21b35501ccfb32c00d5cca1
Pierre Segalen <pierre.segalen@gmail.com>
2022-01-06 11:39:06 +0100

    Prevent Google from bugging me about firebase ids being public

--- 4-PlacesContext/src/firebase.js
+++ 4-PlacesContext/src/firebase.js
@@ -7,13 +7,13 @@ import {
 import { Alert } from "react-native";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB1xLksThm1NBVcDmoxEyT74bYudDTXWWI",
-  authDomain: "ynov-todos.firebaseapp.com",
-  projectId: "ynov-todos",
-  storageBucket: "ynov-todos.appspot.com",
-  messagingSenderId: "659421845328",
-  appId: "1:659421845328:web:a0f0a38ee6536c1aad3e55",
-  measurementId: "G-PNNSYB0H7X",
+  apiKey: "XXXXXXXXXXXXXXXX",
+  authDomain: "xxxxxxxxxxxxxxxxxxx",
+  projectId: "xxxxx",
+  storageBucket: "xxxxxxxxxxxxxxxxxxxxxxx",
+  messagingSenderId: "xxxxxxxxxxxxxxxxxx",
+  appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  measurementId: "xxxxxxxxxxx",
 };
 
 if (getApps().length === 0) {
