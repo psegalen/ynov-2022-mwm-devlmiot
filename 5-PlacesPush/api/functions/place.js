95deb79b9b988fdcc78e2611bb956a100efce747
Pierre Segalen <pierre.segalen@gmail.com>
2022-01-07 11:00:36 +0100

    5th day - Modified API to send push notifications when a place is created

--- 5-PlacesPush/api/functions/place.js
+++ 5-PlacesPush/api/functions/place.js
@@ -1,4 +1,5 @@
 const admin = require("firebase-admin");
+const axios = require("axios");
 
 const printError = (response, err) => {
   functions.logger.error("/place failed", err);
@@ -36,14 +37,52 @@ const deletePlace = (placeId, response) => {
     .catch((err) => printError(response, err));
 };
 
+const sendPushToOthers = (place) => {
+  const usersCollection = admin.firestore().collection("users");
+  usersCollection.get().then((snapshot) => {
+    const users = [];
+    snapshot.forEach((userDoc) => {
+      const user = userDoc.data();
+      users.push(user);
+    });
+    const creator = users.find((u) => u.id === place.userId);
+    const others = users.filter((u) => u.id !== place.userId);
+    others.forEach((user) => {
+      if (!user.token) return;
+      const pushTitle = `Nouveau lieu par ${
+        creator.name || `Utilisateur${creator.id}`
+      }`;
+      const pushBody = `Le lieu concerné est : ${place.place.name}`;
+      const { token } = user;
+      const message = {
+        to: token,
+        title: pushTitle,
+        body: pushBody,
+        data: { place },
+      };
+      console.log("Sending a push!", message);
+      axios.post("https://exp.host/--/api/v2/push/send", message, {
+        headers: {
+          Accept: "application/json",
+          "Accept-encoding": "gzip, deflate",
+          "Content-Type": "application/json",
+        },
+      });
+    });
+  });
+};
+
 const place = (request, response) => {
   const placeId = request.query.placeId;
   if (!placeId) response.send("PlaceId is missing");
   else {
     switch (request.method) {
       case "POST":
+        console.log("Creating place...");
         // Create Place
         createOrUpdatePlace(placeId, request.body, response);
+        // Send push to all users except the creator
+        sendPushToOthers(request.body);
         break;
       case "PATCH":
         // Update Place
